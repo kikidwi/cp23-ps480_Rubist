5619e156ad9184e8f1bed24fd78d03ce5b561349
kiki <kikidprasetyo29@gmail.com>
2023-05-26 18:29:12 +0700

    first commit

---
+++ app/src/main/java/com/cp23ps480/rubist/Login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.cp23ps480.rubist.Login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cp23ps480.rubist.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
---
+++ app/src/main/java/com/cp23ps480/rubist/Register/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.cp23ps480.rubist.Register
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.cp23ps480.rubist.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
