a39a125cea8b6f1b662f5a85d9d9064c37c5a18d
kiki <kikidprasetyo29@gmail.com>
2023-05-30 04:34:58 +0700

    update login-register-forgetpass

--- app/src/main/java/com/cp23ps480/rubist/Fragment/Profile.kt
+++ app/src/main/java/com/cp23ps480/rubist/Fragment/Profile.kt
@@ -5,56 +5,53 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.app.AlertDialog
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import com.cp23ps480.rubist.MainViewModel
 import com.cp23ps480.rubist.R
+import com.cp23ps480.rubist.ViewModel.ProfileViewModel
+import kotlinx.coroutines.launch
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [Profile.newInstance] factory method to
- * create an instance of this fragment.
- */
-class Profile : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class Profile : Fragment(), View.OnClickListener  {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var mainViewModel : MainViewModel
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        mainViewModel = ViewModelProvider(requireActivity()).get(MainViewModel::class.java)
+        val btnLogout : Button = view.findViewById(R.id.logout)
+        btnLogout.setOnClickListener(this)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
+    private fun logoutDialog() {
+        val dialogMessage = getString(R.string.logout_msg)
+        val dialogTitle = getString(R.string.logout)
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Profile.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Profile().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+
+        val alertDialogBuilder = AlertDialog.Builder(requireActivity())
+        alertDialogBuilder.setTitle(dialogTitle)
+
+        alertDialogBuilder
+            .setMessage(dialogMessage)
+            .setCancelable(false)
+            .setPositiveButton(getString(R.string.yes)) { _, _ ->
+              mainViewModel.logout()
             }
+            .setNegativeButton(getString(R.string.No)) { dialog, _ -> dialog.cancel() }
+        val alertDialog = alertDialogBuilder.create()
+        alertDialog.show()
     }
+
+    override fun onClick(v: View?) {
+        logoutDialog()
+    }
+
+
 }
--- app/src/main/java/com/cp23ps480/rubist/MainActivity.kt
+++ app/src/main/java/com/cp23ps480/rubist/MainActivity.kt
@@ -1,23 +1,53 @@
 package com.cp23ps480.rubist
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.cp23ps480.rubist.Fragment.AddPost
 import com.cp23ps480.rubist.Fragment.Community
 import com.cp23ps480.rubist.Fragment.Home
 import com.cp23ps480.rubist.Fragment.Profile
+import com.cp23ps480.rubist.Login.LoginActivity
+import com.cp23ps480.rubist.Model.UserPreference
 import com.cp23ps480.rubist.databinding.ActivityMainBinding
 
+private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "Setting")
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
+    private lateinit var mainViewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mainViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore))
+        )[MainViewModel::class.java]
+
+
+        setupViewModel()
+
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val transaction = fragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout, fragment)
+        transaction.commit()
+    }
+
+    private fun NavBar(){
         replaceFragment(Home())
         binding.BottomNavView.setOnItemSelectedListener{
             when (it.itemId) {
@@ -31,11 +61,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun setupViewModel() {
+        mainViewModel = ViewModelProvider(
+            this,
+            ViewModelFactory(UserPreference.getInstance(dataStore))
+        )[MainViewModel::class.java]
 
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.frame_layout, fragment)
-        transaction.commit()
+        mainViewModel.getUser().observe(this) { user ->
+            if (user.isLogin) {
+                NavBar()
+            } else {
+                startActivity(Intent(this, LoginActivity::class.java))
+                finish()
+            }
+        }
     }
+
 }
