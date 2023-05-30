2e6da5d72b4bd3e872bd02e345d77bb9730852c9
kiki <kikidprasetyo29@gmail.com>
2023-05-30 21:34:09 +0700

    update dikit

--- app/src/main/java/com/cp23ps480/rubist/ForgetPassword/ForgetPasswordActivity.kt
+++ app/src/main/java/com/cp23ps480/rubist/ForgetPassword/ForgetPasswordActivity.kt
@@ -3,15 +3,18 @@ package com.cp23ps480.rubist.ForgetPassword
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import com.cp23ps480.rubist.API.Request.UserRequest
 import com.cp23ps480.rubist.API.Response.ChangePassResponse
+import com.cp23ps480.rubist.API.Response.LoginResponse
 import com.cp23ps480.rubist.API.Retrofit.ApiConfig
 import com.cp23ps480.rubist.MainActivity
 import com.cp23ps480.rubist.R
 import com.cp23ps480.rubist.Register.RegisterActivity
 import com.cp23ps480.rubist.databinding.ActivityForgetPasswordBinding
+import com.google.gson.Gson
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -26,6 +29,7 @@ class ForgetPasswordActivity : AppCompatActivity() {
 
         binding.btnSend.setOnClickListener{
             val email = binding.etEmail.text.toString()
+            setLoading(true)
             Send(email)
         }
     }
@@ -39,7 +43,7 @@ class ForgetPasswordActivity : AppCompatActivity() {
                 response: Response<ChangePassResponse>
             ) {
                 if (response.isSuccessful) {
-                    //setLoading(false)
+                    setLoading(false)
                     val responseBody = response.body()
                     if (responseBody != null) {
                         AlertDialog.Builder(this@ForgetPasswordActivity).apply {
@@ -53,12 +57,36 @@ class ForgetPasswordActivity : AppCompatActivity() {
                         }
                     }
                 }
+                else {
+                    setLoading(false)
+                    val responseBody = Gson().fromJson(
+                        response.errorBody()?.charStream(),
+                        LoginResponse::class.java
+                    )
+                    AlertDialog.Builder(this@ForgetPasswordActivity).apply {
+                        setTitle("Error!")
+                        setMessage(responseBody.error)
+                        setPositiveButton("OK") { _, _ ->
+                            finish()
+                        }
+                        create()
+                        show()
+                    }
+                }
             }
 
             override fun onFailure(call: Call<ChangePassResponse>, t: Throwable) {
-                //setLoading(false)
+                setLoading(false)
                 Toast.makeText(this@ForgetPasswordActivity, t.message, Toast.LENGTH_SHORT).show()
             }
         })
+    }
+
+    fun setLoading(isLoading : Boolean){
+        if (isLoading){
+            binding.loading.visibility = View.VISIBLE
+        }else{
+            binding.loading.visibility = View.GONE
+        }
     }
 }
--- app/src/main/java/com/cp23ps480/rubist/MainActivity.kt
+++ app/src/main/java/com/cp23ps480/rubist/MainActivity.kt
@@ -1,28 +1,45 @@
 package com.cp23ps480.rubist
 
+import android.content.ActivityNotFoundException
 import android.content.Context
+import android.Manifest
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import androidx.datastore.core.DataStore
 import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.cp23ps480.rubist.AddPost.AddPostActivity
 import com.cp23ps480.rubist.Fragment.AddPost
 import com.cp23ps480.rubist.Fragment.Community
 import com.cp23ps480.rubist.Fragment.Home
 import com.cp23ps480.rubist.Fragment.Profile
 import com.cp23ps480.rubist.Login.LoginActivity
 import com.cp23ps480.rubist.Model.UserPreference
+import com.cp23ps480.rubist.Utils.createCustomTempFile
 import com.cp23ps480.rubist.databinding.ActivityMainBinding
+import java.io.File
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "Setting")
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding : ActivityMainBinding
     private lateinit var mainViewModel : MainViewModel
+    private lateinit var currentPhotoPath: String
+    private var getFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +53,17 @@ class MainActivity : AppCompatActivity() {
 
 
         setupViewModel()
+
+        if (!allPermissionsGranted()) {
+            ActivityCompat.requestPermissions(
+                this,
+                REQUIRED_PERMISSIONS,
+                REQUEST_CODE_PERMISSIONS
+            )
+        }
+        binding.camera.setOnClickListener{
+            startTakePhoto()
+        }
 
     }
 
@@ -53,7 +81,7 @@ class MainActivity : AppCompatActivity() {
             when (it.itemId) {
                 R.id.home -> replaceFragment(Home())
                 R.id.community -> replaceFragment(Community())
-                R.id.add -> replaceFragment(AddPost())
+                R.id.add -> startActivity(Intent(this, AddPostActivity::class.java))
                 R.id.profile-> replaceFragment(Profile())
                 else -> {}
             }
@@ -75,6 +103,48 @@ class MainActivity : AppCompatActivity() {
                 finish()
             }
         }
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
+    }
+
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (!allPermissionsGranted()) {
+                Toast.makeText(
+                    this,
+                    "Tidak mendapatkan permission.",
+                    Toast.LENGTH_SHORT
+                ).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private val launcherIntentCamera = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) {
+        if (it.resultCode == RESULT_OK) {
+            val imageBitmap = it.data?.extras?.get("data") as Bitmap
+//            binding.previewImageView.setImageBitmap(imageBitmap)
+        }
+    }
+
+    private fun startTakePhoto() {
+        val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        launcherIntentCamera.launch(intent)
     }
 
 }
