6d4fd51a81ea3228c38ad50b3fdf6075a4cbb721
kiki <kikidprasetyo29@gmail.com>
2023-06-02 19:15:22 +0700

    Camera for upload trash image (udah dicoba di API dicoding dan aman)

--- app/src/main/java/com/cp23ps480/rubist/MainActivity.kt
+++ app/src/main/java/com/cp23ps480/rubist/MainActivity.kt
@@ -22,7 +22,10 @@ import androidx.datastore.preferences.core.Preferences
 import androidx.datastore.preferences.preferencesDataStore
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.cp23ps480.rubist.API.Response.FileUploadResponse
+import com.cp23ps480.rubist.API.Retrofit.ApiConfig
 import com.cp23ps480.rubist.AddPost.AddPostActivity
+import com.cp23ps480.rubist.AfterScan.AfterScanActivity
 import com.cp23ps480.rubist.Fragment.AddPost
 import com.cp23ps480.rubist.Fragment.Community
 import com.cp23ps480.rubist.Fragment.Home
@@ -30,7 +33,15 @@ import com.cp23ps480.rubist.Fragment.Profile
 import com.cp23ps480.rubist.Login.LoginActivity
 import com.cp23ps480.rubist.Model.UserPreference
 import com.cp23ps480.rubist.Utils.createCustomTempFile
+import com.cp23ps480.rubist.Utils.reduceFileImage
 import com.cp23ps480.rubist.databinding.ActivityMainBinding
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody.Companion.asRequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.io.File
 
 private val Context.dataStore: DataStore<Preferences> by preferencesDataStore(name = "Setting")
@@ -106,6 +117,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     companion object {
+
         private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
         private const val REQUEST_CODE_PERMISSIONS = 10
     }
@@ -137,14 +149,79 @@ class MainActivity : AppCompatActivity() {
         ActivityResultContracts.StartActivityForResult()
     ) {
         if (it.resultCode == RESULT_OK) {
-            val imageBitmap = it.data?.extras?.get("data") as Bitmap
-//            binding.previewImageView.setImageBitmap(imageBitmap)
+            val myFile = File(currentPhotoPath)
+            getFile = myFile
+            uploadImage()
+            AfterScanActivity.Image = getFile
+            startActivity(Intent(this, AfterScanActivity::class.java))
+
+//            myFile.let { file ->
+////          Silakan gunakan kode ini jika mengalami perubahan rotasi
+////          rotateFile(file)
+//                binding.previewImageView.setImageBitmap(BitmapFactory.decodeFile(file.path))
+//            }
         }
     }
 
     private fun startTakePhoto() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        launcherIntentCamera.launch(intent)
+        intent.resolveActivity(packageManager)
+
+        createCustomTempFile(application).also {
+            val photoURI: Uri = FileProvider.getUriForFile(
+                this@MainActivity,
+                "com.cp23ps480.rubist",
+                it
+            )
+            currentPhotoPath = it.absolutePath
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI)
+            launcherIntentCamera.launch(intent)
+        }
+    }
+
+    private fun uploadImage() {
+//        setLoading(true)
+
+
+        if (getFile != null) {
+            val file = reduceFileImage(getFile as File)
+
+            val description = binding.etDesc.text.toString().toRequestBody("text/plain".toMediaType())
+            val requestImageFile = file.asRequestBody("image/jpeg".toMediaType())
+            val imageMultipart: MultipartBody.Part = MultipartBody.Part.createFormData(
+                "photo",
+                file.name,
+                requestImageFile
+            )
+
+            val service = ApiConfig.getApiService().uploadImage(imageMultipart, description)
+
+            service.enqueue(object : Callback<FileUploadResponse> {
+                override fun onResponse(
+                    call: Call<FileUploadResponse>,
+                    response: Response<FileUploadResponse>
+                ) {
+                    if (response.isSuccessful) {
+//                        setLoading(false)
+                        val responseBody = response.body()
+                        if (responseBody != null && !responseBody.error!!) {
+                            Toast.makeText(this@MainActivity, responseBody.message, Toast.LENGTH_SHORT).show()
+//                            finish()
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<FileUploadResponse>, t: Throwable) {
+//                    setLoading(false)
+                    Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
+                }
+            })
+
+
+        } else {
+//            setLoading(false)
+            Toast.makeText(this@MainActivity, "Silakan masukkan berkas gambar terlebih dahulu.", Toast.LENGTH_SHORT).show()
+        }
     }
 
 }
